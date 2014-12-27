7956a169ef74467ffab37e7c234e87ecaba2f83e
ShyneColdchain <olivermuellerklein@gmail.com>
2014-12-27 14:00:32 -0500

    gbt setup

--- titanic.test.py
+++ titanic.test.py
@@ -1,9 +1,19 @@
 import pandas as pd
 import numpy as np
 
+from sklearn.ensemble import GradientBoostingClassifier 
+
 data = pd.read_csv('train.csv', dtype = str)
-survive = data['Survived']
+# .values = convert to np
+survive = data['Survived'].astype(int).values
 data = data.drop('Survived', 1)
 
 for colname in list(data.columns.values):
 	data[colname] = pd.Categorical.from_array(data[colname]).codes
+
+# convert to np    
+data = data.values
+ 
+# gradient boosting     
+gbt = GradientBoostingClassifier(verbose = 1)
+gbt.fit(data, survive)
